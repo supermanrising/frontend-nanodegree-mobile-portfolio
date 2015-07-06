5071a2fa34bb0927ec6211cd92cd8a2371187817
Ryan Vrba <ryan.vrba@gmail.com>
2015-07-06 14:07:40 +0200

    Added gulp modules for minfying HTML and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require('gulp');
  
 // include plug-ins
 var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
  
 // JS hint task
 gulp.task('jshint', function() {
@@ -10,9 +14,6 @@ gulp.task('jshint', function() {
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
-
-var changed = require('gulp-changed');
-var imagemin = require('gulp-imagemin');
 
 // minify new images
 gulp.task('imagemin', function() {
@@ -23,4 +24,26 @@ gulp.task('imagemin', function() {
     .pipe(changed(imgDst))
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
+});
+ 
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+  var htmlSrc = './*.html',
+      htmlDst = './minified';
+ 
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+// minify new or changed HTML pages
+gulp.task('cssmin', function() {
+  var cssSrc = './css/*.css',
+      cssDst = './minified/css';
+ 
+  gulp.src(cssSrc)
+    .pipe(changed(cssDst))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(cssDst));
 });
