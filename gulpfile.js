9a89a73dc062d4815ce35f94f0c90fd6a24a11da
Ryan Vrba <ryan.vrba@gmail.com>
2015-07-07 15:19:35 +0200

    Modified gulpfile.js to create minified version of pizza section

--- gulpfile.js
+++ gulpfile.js
@@ -29,11 +29,33 @@ gulp.task('imagemin', function() {
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
 });
+
+// minify new images for pizza.html
+gulp.task('views-imagemin', function() {
+  var imgSrc = './views/images/*',
+      imgDst = './minified/views/images';
+ 
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
  
 // minify new or changed HTML pages
 gulp.task('htmlpage', function() {
   var htmlSrc = './*.html',
       htmlDst = './minified';
+ 
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+// minify new or changed HTML pages for pizza.html
+gulp.task('views-html', function() {
+  var htmlSrc = './views/*.html',
+      htmlDst = './minified/views';
  
   gulp.src(htmlSrc)
     .pipe(changed(htmlDst))
@@ -52,10 +74,33 @@ gulp.task('cssmin', function() {
     .pipe(gulp.dest(cssDst));
 });
 
+// minify new or changed CSS pages
+gulp.task('views-cssmin', function() {
+  var cssSrc = './views/css/*.css',
+      cssDst = './minified/views/css';
+ 
+  gulp.src(cssSrc)
+    .pipe(changed(cssDst))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(cssDst));
+});
+
 // JS strip debugging and minify
 gulp.task('scripts', function() {
   var jsSrc = './js/*.js',
       jsDst = './minified/js';
+
+  gulp.src(jsSrc)
+    .pipe(changed(jsDst))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDst));
+});
+
+// JS strip debugging and minify
+gulp.task('views-scripts', function() {
+  var jsSrc = './views/js/*.js',
+      jsDst = './minified/views/js';
 
   gulp.src(jsSrc)
     .pipe(changed(jsDst))
